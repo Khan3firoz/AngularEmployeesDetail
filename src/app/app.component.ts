f8fb775e379fb0962c12d75a70b6a5bc0aefb0e3
Firoz <khan3firoz@gmail.com>
2021-02-07 17:25:59 +0530

    input output by parent and child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employe';
+  employees: any[];
+
+  constructor() {
+    this.employees = [
+      { empId: '10001', fname: 'Firoz', lname: 'Khan', dep: 'Web Devloper', city: 'Bengaluru', email: 'khan3firoz@gmail.com' },
+      { empId: '10002', fname: 'Akhlaque', lname: 'Ahmad', dep: 'Web Devloper', city: 'Bengaluru', email: 'akhlaquea01@gmail.com' },
+      { empId: '10003', fname: 'Ashfaque', lname: 'Ahmad', dep: 'Data Scientist', city: 'Bengaluru', email: 'ashfaque786@gmail.com' },
+      { empId: '10004', fname: 'Krishna', lname: 'kumar', dep: 'Mechanical', city: 'Noida', email: 'twariji@gmail.com' }
+    ]
+  }
+  addEventHandler(event: any) {
+    this.employees.push(event)
+  }
+
+  addEmployee() {
+    alert('you add a data');
+    this.employees.push({ empId: '10004', fname: 'Krishna', lname: 'kumar', dep: 'Mechanical', city: 'Noida', email: 'twariji@gmail.com' })
+  }
 }
